e0add27d30a4dfbb30942368858b9de03f5aa3c3
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-01-24 12:48:51 -0500

    Initial init

---
+++ react-test-app1/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CourseGoals from './components/CourseGoals.tsx';
+
+function App() {
+
+  return (
+    <main>
+      <CourseGoals/>
+    </main>
+  )
+}
+
+export default App
---
+++ react-test-app1/src/components/CourseGoals.tsx
@@ -0,0 +1,11 @@
+function CourseGoals() {
+    return <article>
+        <div>
+            <h2>TITLE</h2>
+            <p>DESCRIPTION</p>
+        </div>
+        <button>Delete</button>
+    </article>
+}
+
+export default CourseGoals
