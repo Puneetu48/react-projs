da74ce4174d9f2275dc7ce1796f7028e97fd7c13
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-01-24 13:38:52 -0500

    Using state

--- react-test-app1/src/App.tsx
+++ react-test-app1/src/App.tsx
@@ -1,21 +1,46 @@
-import './App.css'
-import goalsImg from './assets/goals.jpg';
-import CourseGoals from './components/CourseGoals.tsx';
-import Header from './components/Header.tsx';
+import { useState } from "react";
+import "./App.css";
+import goalsImg from "./assets/goals.jpg";
+import CourseGoals from "./components/CourseGoals.tsx";
+import Header from "./components/Header.tsx";
+
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+}
 
 function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    setGoals(prevGoals => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: 'Learn React with TS',
+        description: 'Deep dive learning'
+      };
+      return [...prevGoals, newGoal]
+    });
+  }
 
   return (
     <main>
-      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoals
-        title="Learn React with TS">
-        <p> Refresh your react knowledge with TS</p>
-      </CourseGoals>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoals title={goal.title}>
+              {goal.description}
+            </CourseGoals>
+          </li>
+        ))}
+      </ul>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
