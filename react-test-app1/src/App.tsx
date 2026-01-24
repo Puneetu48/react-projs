2e19d20d1cac3dca0dda6cfa8ab749ed82fa4a8e
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-01-24 13:21:35 -0500

    Add a header component

--- react-test-app1/src/App.tsx
+++ react-test-app1/src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css'
+import goalsImg from './assets/goals.jpg';
 import CourseGoals from './components/CourseGoals.tsx';
+import Header from './components/Header.tsx';
 
 function App() {
 
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoals
         title="Learn React with TS">
-          <p> Refresh your react knowledge with TS</p>
-        </CourseGoals>
+        <p> Refresh your react knowledge with TS</p>
+      </CourseGoals>
     </main>
   )
 }
