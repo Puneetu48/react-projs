e5171f8c02d97f24fc0d6bb0a097756f6cf7324b
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-01-26 08:51:32 -0500

    Using a reusable component

--- react-test-app1/src/components/CourseGoalsList.tsx
+++ react-test-app1/src/components/CourseGoalsList.tsx
@@ -1,21 +1,36 @@
 import CourseGoals from "./CourseGoals";
-import {type CourseGoal as CGoal} from "../App.tsx";
+import { type CourseGoal as CGoal } from "../App.tsx";
+import InfoBox from "./InfoBox.tsx";
+import type { ReactNode } from "react";
 
 type CourseGoalListProps = {
     goals: CGoal[]
     onDeleteGoal: (id: number) => void;
 }
 
-export default function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
+export default function CourseGoalList({
+    goals,
+    onDeleteGoal
+}: CourseGoalListProps) {
+    if (goals.length === 0) {
+        return <InfoBox mode="hint">You have no course goals yet. Start adding some!</InfoBox>
+    }
+    let warningBox: ReactNode;
+    if (goals.length >= 4) {
+        warningBox = <InfoBox severity="high" mode="warning">You are adding too many goals. Don't add so much bro. Chill!</InfoBox>
+    }
     return (
-        <ul>
-            {goals.map((goal) => (
-                <li key={goal.id}>
-                    <CourseGoals id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-                        {goal.description}
-                    </CourseGoals>
-                </li>
-            ))}
-        </ul>
+        <>
+            {warningBox}
+            <ul>
+                {goals.map((goal) => (
+                    <li key={goal.id}>
+                        <CourseGoals id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+                            {goal.description}
+                        </CourseGoals>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
---
+++ react-test-app1/src/components/InfoBox.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react"
+
+type InfoBoxProps = {
+    mode: 'hint' | 'warning';
+    severity?: 'low' | 'medium' | 'high';
+    children: ReactNode;
+}
+
+export default function InfoBox({ mode, severity, children }: InfoBoxProps) { //info, warning
+    if (mode === 'hint') {
+        return (<aside className="infobox infobox-hint">
+            <p>{children}</p>
+        </aside>);
+    }
+
+    return (<aside className={`infobox infobox-warning warning--${severity}`}>
+        <h2>Warning</h2>
+        <p>{children}</p>
+    </aside>)
+}
