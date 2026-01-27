b9408de82670702242368ebb3d28dc5a3a015eb5
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-01-27 16:06:33 -0500

    Building better wrapper component with ComponentPropsWithoutRef

--- react-app1-advanced/src/App.tsx
+++ react-app1-advanced/src/App.tsx
@@ -3,8 +3,8 @@ import Input from "./component/Input";
 function App() {
   return (
     <>
-      <Input id="name" label="Your name" />
-      <Input id="age" label="Your age" />
+      <Input id="name" label="Your name" type="string" />
+      <Input id="age" label="Your age" type="number" />
     </>
   )
 }
