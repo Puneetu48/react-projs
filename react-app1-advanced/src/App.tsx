ddef96ca07a3c46a506153eec8ca951d109f8808
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-01-28 15:47:46 -0500

    using forward ref

--- react-app1-advanced/src/App.tsx
+++ react-app1-advanced/src/App.tsx
@@ -1,11 +1,12 @@
-import Button from "./component/Button";
-import Container from "./component/Container";
+import { useRef } from "react";
+import Input from "./component/Input";
 
 function App() {
+  const input = useRef<HTMLInputElement>(null);
   return (
     <>
       <main>
-        <Container el="anchor" as={Button}>Click me</Container>
+        <Input label="What day is it?" id="inp1" ref={input}></Input>
       </main>
     </>
   )
