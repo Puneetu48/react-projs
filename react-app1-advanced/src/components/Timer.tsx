5d6173b2aca74269464be2521920bcbca6d823b4
Puneet Upadhyay <48835271+Puneetu48@users.noreply.github.com>
2026-02-02 00:08:13 -0500

    Using effect for stopping re rendering

--- react-app1-advanced/src/components/Timer.tsx
+++ react-app1-advanced/src/components/Timer.tsx
@@ -1,16 +1,48 @@
 import Container from "./UI/Container";
-import { type Timer as TimerProps } from "../store/timers-context";
-
-// type TimerProps = {
-//   name: string;
-//   duration: number;
-// };
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timers-context";
+import { useEffect, useRef, useState } from "react";
 
 export default function Time1r({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  const { isRunning } = useTimersContext();
+
+  useEffect(() => {
+    if (remainingTime <= 0 && interval.current) {
+      clearInterval(interval.current);
+    }
+    let timer: number;
+
+    if (isRunning) {
+      timer = interval.current = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime;
+          }
+          return prevTime - 50;
+        });
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning, remainingTime]);
+
+  const formatterRemainingTime = (remainingTime / 1000).toFixed(2);
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime}></progress>
+      </p>
+      <p>{formatterRemainingTime}</p>
     </Container>
   );
 }
